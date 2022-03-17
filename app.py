11d06bac6d45a793da97d07f21542247d2e68ade
glowinthedark <48893368+glowinthedark@users.noreply.github.com>
2022-03-18 00:09:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+from pathlib import Path
 
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
@@ -11,20 +13,24 @@ def upload_file():
     return render_template('index.html')
 
 
-@app.route('/display', methods = ['GET', 'POST'])
+@app.route('/display', methods=['GET', 'POST'])
 def save_file():
+    content = "error"
+    
     if request.method == 'POST':
-        f = request.files['file']
-        filename = secure_filename(f.filename)
+        fileStorage = request.files['file']
+        filename = secure_filename(fileStorage.filename)
 
-        f.save(app.config['UPLOAD_FOLDER'] + filename)
+        dest = Path(app.config['UPLOAD_FOLDER'])/filename
+        dest.parent.mkdir(exist_ok=True, parents=True)
+        
+        with dest.open('wb') as fo:
+            fileStorage.save(fo)
 
-        file = open(app.config['UPLOAD_FOLDER'] + filename,"r")
-        content = file.read()
-        
-        
-    return render_template('content.html', content=content) 
+        content = dest.read_text(encoding='utf-8')
+
+    return render_template('content.html', content=content)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug = True)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
